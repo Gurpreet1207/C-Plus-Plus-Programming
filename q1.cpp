56e883a11f9ca6570a33120a5c4bba1b9ba3867d
Gurpreet kaur <singhbajinder32@gmail.com>
2026-07-02 10:15:19 +0530

    Add files via upload

--- q1.cpp
+++ q1.cpp
@@ -1,30 +1,36 @@
-// special member function used to initializes data members
-//it is called automatically when we create objects
-//default 
+//Method chaning is used when to call multiple function in a single line
 #include<iostream>
 using namespace std;
-class data{
-    private:
-    int a;
-    float b;
+class calculator{
+    int value;
     public:
-    data()
+    calculator()//constructer 
     {
-        a=10;
-        b=50.5;
-        cout<<"Constructor Called."<<endl;
+        value=0;
     }
-    void show()
+    calculator & add(int x) //making of function for method chaning
     {
-        cout<<"a= "<<a<<endl;
-        cout<<"b= "<<b<<endl;
+        value=value+x;
+        cout<<"Value after addition: "<<value<<endl;
+        return*this;  //imp to add if using method chaning
     }
-
+    calculator & sub(int y){
+        value=value-y;
+        cout<<"Value after Substraction: "<<value<<endl;
+         return*this;
+    }
+    calculator & mul(int z){
+value=value*z;
+cout<<"Value after Multiplication: "<<value<<endl;
+ return*this;
+    }
+    void show(){
+        cout<<"After all calculation value: "<<value<<endl;
+        cout<<"Method Chaining";
+    }
 };
-int main()
-{
-    data obj;
-    obj.show();
-
+int main(){
+    calculator c1;
+    c1.add(50).sub(30).mul(10).show();  //calling of multiple functions in one go
     return 0;
 }
